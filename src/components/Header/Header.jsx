7b279ea3fbbb9288540dd79a621c92da2bec958a
Daphaz <bonnet.damien@ymail.com>
2021-02-16 16:50:14 +0100

    feat : navigation menu
    add a little hover effect on burger icon and use
    animjs for transition
    Closes #7

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,21 +1,68 @@
-import React from "react";
+import React, { useState } from "react";
 import Logo from "../../../assets/images/logo.png";
+import animjs from "animejs";
 
-const Header = () => {
+const Header = ({ dimensions }) => {
+	const [toggle, setToggle] = useState(false);
+	const handleMenu = () => {
+		const ovMenu = document.querySelector(".ov-menu");
+		const menu = document.querySelector(".menu");
+
+		let size = dimensions.width <= 425 ? 100 : 200;
+
+		const bodyOverflow = () => {
+			document.body.classList.toggle("menu-open");
+		};
+
+		const tl = animjs.timeline({
+			duration: 450,
+			easing: "easeOutExpo",
+			begin: bodyOverflow,
+		});
+
+		const displayMenu = () => {
+			if (!toggle) {
+				menu.style.display = "flex";
+			} else {
+				menu.style.display = "none";
+			}
+		};
+
+		const translateEl = (el) => {
+			el.style.transform = "translate(-50%,-50%)";
+		};
+
+		tl.add({
+			targets: ovMenu,
+			scale: size,
+			complete: displayMenu,
+			begin: translateEl(ovMenu),
+		}).add({
+			targets: ovMenu,
+			scale: 0.001,
+			begin: translateEl(ovMenu),
+		});
+
+		setToggle(!toggle);
+	};
+
 	return (
 		<header className="header">
-			<a href="/">
+			<a href="/" className="logo">
 				<img src={Logo} alt="logo" />
 			</a>
 			<div className="links-menu">
 				<a href="/">Login</a>
-				<a href="/">
+				<div className="btn-menu">
 					<p>Menu</p>
-					<div className="burger">
-						<span></span>
-						<span></span>
+					<div className="burger" onClick={handleMenu}>
+						<div className="ov-menu"></div>
+						<div className="icon-menu">
+							<span></span>
+							<span></span>
+						</div>
 					</div>
-				</a>
+				</div>
 			</div>
 		</header>
 	);
